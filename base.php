6d2bf2c2dbdd0c20cdce8c8ff24cf235d1296362
l6qiang <liuqianglan@sina.com>
2020-12-22 18:04:09 +0800

    1233

--- base.php
+++ base.php
@@ -2,9 +2,9 @@
 
 class Base
 {
-    static $redisObj;
+    private static $redisObj;
 
-    static function conRedis($config = array())
+    public static function conRedis($config = array())
     {
         if (self::$redisObj) return self::$redisObj;
         self::$redisObj = new \Redis();
@@ -19,6 +19,25 @@ class Base
         $res['data'] = $data;
         echo json_encode($res);exit();
     }
+
+    private function __clone()
+    {
+
+    }
 }
 
+/**
+ * 两个对象是同一个对象时才全等
+ * 方法前加final，则方法不能被覆盖；类前加final，则类不能被继承
+ *
+ * 面向对象的三大特性：封装、继承、多态
+ */
+
+$s1 = Base::conRedis();
+$s2 = clone $s1;
+if ($s1 === $s2) {
+    echo '是同一个对象';
+} else {
+    echo '不是同一个对象';
+}
 ?>
